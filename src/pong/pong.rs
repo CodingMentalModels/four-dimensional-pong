1c7169c71f26d92d509377a7495cfe751dbb2bd2
codingmentalmodels <codingmentalmodels@gmail.com>
2022-10-13 11:16:52 -0400

    Handle goal collisions

--- src/pong/pong.rs
+++ src/pong/pong.rs
@@ -4,9 +4,12 @@ use rand::{seq::SliceRandom, distributions::Standard};
 use bevy::{prelude::*, window::{PresentMode}, gltf::{Gltf, GltfMesh}, asset::LoadState};
 use iyes_loopless::prelude::*;
 
+use crate::pong::player::Player;
+
 const GLTF_PATH: &str = "pong.glb";
-const ARENA_LENGTH: f32 = 2.0;
+const ARENA_LENGTH: f32 = 5.0;
 const PADDLE_STARTING_OFFSET: f32 = 0.5;
+const GOAL_OFFSET_FROM_ARENA: f32 = 0.1;
 
 
 pub struct PongPlugin;
@@ -28,13 +31,16 @@ impl Plugin for PongPlugin {
                 brightness: 1.0 / 2.0,
             }
         ).insert_resource(Time::default())
+        .add_event::<ScoreEvent>()
         .add_loopless_state(PongState::LoadingAssets)
         .add_startup_system(load_gltf)
         .add_system(stage_load_system.run_in_state(PongState::LoadingAssets))
         .add_enter_system(PongState::InGame, ui_load_system)
         .add_enter_system(PongState::InGame, ball_initial_velocity_system)
         .add_system(ball_movement_system.run_in_state(PongState::InGame))
-        .add_system(render_system.run_in_state(PongState::InGame));
+        .add_system(collision_system.run_in_state(PongState::InGame))
+        .add_system(render_system.run_in_state(PongState::InGame))
+        .add_system(score_system.run_in_state(PongState::InGame));
     }
 }
 
@@ -59,7 +65,8 @@ struct GltfModel(Handle<Gltf>);
 
 // Events
 
-
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct ScoreEvent(Player);
 
 // End Events
 
@@ -90,6 +97,12 @@ struct MaterialHandleComponent(Handle<StandardMaterial>);
 
 #[derive(Component)]
 struct NeedsRenderingComponent;
+
+#[derive(Component)]
+struct PlayerScoreComponent;
+
+#[derive(Component)]
+struct OpponentScoreComponent;
 
 // End Components
 
@@ -152,8 +165,8 @@ fn stage_load_system(
         .insert(NeedsRenderingComponent);
 
         
-        let player_starting_position = Vec4::new(0., 0., -PADDLE_STARTING_OFFSET, -ARENA_LENGTH);
-        let opponent_starting_position = Vec4::new(0., 0., PADDLE_STARTING_OFFSET, ARENA_LENGTH);
+        let player_starting_position = Vec4::new(0., 0., -PADDLE_STARTING_OFFSET, -(ARENA_LENGTH / 2.));
+        let opponent_starting_position = Vec4::new(0., 0., PADDLE_STARTING_OFFSET, (ARENA_LENGTH / 2.0));
 
         commands.spawn_bundle(
             PbrBundle {
@@ -230,7 +243,7 @@ fn ui_load_system(
                     text_style.clone(),
                     JustifyContent::SpaceBetween,
                 )
-            );
+            ).insert(PlayerScoreComponent);
             parent.spawn_bundle(
                 get_text_bundle(
                     "4D Pong",
@@ -244,7 +257,7 @@ fn ui_load_system(
                     text_style.clone(),
                     JustifyContent::SpaceBetween,
                 )
-            );
+            ).insert(OpponentScoreComponent);
         }
     );
 }
@@ -271,19 +284,46 @@ fn get_text_bundle(
 fn ball_initial_velocity_system(
     mut ball_query: Query<(&mut VelocityComponent), With<BallComponent>>,
 ) {
-    let directions = vec![-1., 1.];
-    let w_velocity = directions.choose(&mut rand::thread_rng()).expect("Directions is never empty.");
     for (mut velocity) in ball_query.iter_mut() {
-        velocity.0 = Vec4::new(0.0, 0.0, 0.0, *w_velocity);
+        velocity.0 = roll_initial_velocity();
     }
 }
 
 fn ball_movement_system(
     time: Res<Time>,
-    mut ball_query: Query<(&mut PositionComponent, &VelocityComponent, &BallComponent)>,
+    mut ball_query: Query<(&mut PositionComponent, &VelocityComponent), With<BallComponent>>,
 ) {
-    for (mut position, velocity, ball) in ball_query.iter_mut() {
+    for (mut position, velocity) in ball_query.iter_mut() {
         position.0 += velocity.0 * time.delta_seconds();
+    }
+}
+
+fn collision_system(
+    mut ball_query: Query<(&mut PositionComponent, &mut VelocityComponent), With<BallComponent>>,
+    mut paddle_query: Query<(&mut PositionComponent, &PaddleComponent), Without<BallComponent>>,
+    mut score_event_writer: EventWriter<ScoreEvent>,
+) {
+    for (mut ball_position, mut ball_velocity) in ball_query.iter_mut() {
+        match is_goal_collision(ball_position.0) {
+            Some(player) => {
+                ball_position.0 = Vec4::new(0., 0., 0., 0.);
+                ball_velocity.0 = roll_initial_velocity();
+
+                score_event_writer.send(ScoreEvent(player));
+            },
+            None => {
+                // Do nothing
+            }
+        }
+        for (mut paddle_position, paddle) in paddle_query.iter_mut() {
+            // if ball_position.0.w > paddle_position.0.w {
+            //     if ball_position.0.x > paddle_position.0.x - PADDLE_WIDTH/2. && ball_position.0.x < paddle_position.0.x + PADDLE_WIDTH/2. {
+            //         if ball_position.0.y > paddle_position.0.y - PADDLE_HEIGHT/2. && ball_position.0.y < paddle_position.0.y + PADDLE_HEIGHT/2. {
+            //             ball_velocity.0.w *= -1.;
+            //         }
+            //     }
+            // }
+        }
     }
 }
 
@@ -304,13 +344,57 @@ fn render_system(
     }
 }
 
+fn score_system(
+    mut score_event_reader: EventReader<ScoreEvent>,
+    mut player_score_query: Query<&mut Text, (With<PlayerScoreComponent>, Without<OpponentScoreComponent>)>,
+    mut opponent_score_query: Query<&mut Text, (With<OpponentScoreComponent>, Without<PlayerScoreComponent>)>,
+) {
+    for score_event in score_event_reader.iter() {
+        match score_event.0 {
+            Player::Blue => {
+                for mut text in player_score_query.iter_mut() {
+                    text.sections[0].value = (text.sections[0].value.parse::<u32>().unwrap() + 1).to_string();
+                }
+            },
+            Player::Red => {
+                for mut text in opponent_score_query.iter_mut() {
+                    text.sections[0].value = (text.sections[0].value.parse::<u32>().unwrap() + 1).to_string();
+                }
+            }
+        }
+    }
+}
+
+
+// End Systems
+
+// Helper Functions
+
+fn roll_initial_velocity() -> Vec4 {
+    let directions = vec![-1., 1.];
+    let w_velocity = directions.choose(&mut rand::thread_rng()).expect("Directions is never empty.");
+    Vec4::new(0.0, 0.0, 0.0, *w_velocity)
+}
+
+fn is_goal_collision(position: Vec4) -> Option<Player> {
+    let goal_distance = (ARENA_LENGTH + GOAL_OFFSET_FROM_ARENA) / 2.;
+    if position.w >= goal_distance {
+        Some(Player::Blue)
+    } else if position.w <= -goal_distance {
+        Some(Player::Red)
+    } else {
+        None
+    }
+}
+
+
 fn get_color_from_w(w: f32, arena_length: f32) -> Color {
     let blue = Color::BLUE.as_hsla_f32();
     let red = Color::RED.as_hsla_f32();
     let saturation = blue[1];
     let lightness = blue[2];
     let alpha = blue[3];
-    let factor = (w + arena_length) / (2.0 * arena_length);
+    let factor = (w + arena_length / 2.) / arena_length;
     Color::Hsla {
         hue: lerp(blue[0], red[0], factor),
         saturation: saturation,
@@ -323,14 +407,33 @@ fn lerp(a: f32, b: f32, t: f32) -> f32 {
     a + (b - a) * t
 }
 
-// End Systems
-
+// End Helper Functions
 
 #[cfg(test)]
 mod test_pong_plugin {
     use bevy::{asset::AssetPlugin, gltf::GltfPlugin};
 
     use super::*;
+
+    fn initialize_pong_plugin_and_load_assets() -> App {
+        let mut app = App::new();
+        app
+            .add_plugins(MinimalPlugins)
+            .add_plugin(AssetPlugin)
+            .add_plugin(GltfPlugin)
+            .add_plugin(PongPlugin)
+            .add_asset::<bevy::pbr::prelude::StandardMaterial>()
+            .add_asset::<bevy::render::prelude::Mesh>()
+            .add_asset::<bevy::scene::Scene>();
+
+
+        app.update();
+        assert!(app.world.contains_resource::<GltfModel>());
+        std::thread::sleep(std::time::Duration::from_millis(100)); // Allow time for assets to load.
+        app.update(); // PongState::LoadingAssets -> PongState::InGame
+
+        return app;
+    }
 
     #[test]
     fn test_assets_load() {
@@ -363,25 +466,12 @@ mod test_pong_plugin {
 
     #[test]
     fn test_pong_plugin_initializes() {
-        let mut app = App::new();
-        app
-            .add_plugins(MinimalPlugins)
-            .add_plugin(AssetPlugin)
-            .add_plugin(GltfPlugin)
-            .add_plugin(PongPlugin)
-            .add_asset::<bevy::pbr::prelude::StandardMaterial>()
-            .add_asset::<bevy::render::prelude::Mesh>()
-            .add_asset::<bevy::scene::Scene>();
-
+        let mut app = initialize_pong_plugin_and_load_assets();
+        
         assert!(app.world.contains_resource::<Time>());
         assert!(app.world.contains_resource::<WindowDescriptor>());
         assert!(app.world.contains_resource::<AmbientLight>());
         
-        app.update();
-        assert!(app.world.contains_resource::<GltfModel>());
-        std::thread::sleep(std::time::Duration::from_millis(100)); // Allow time for assets to load.
-        app.update(); // PongState::LoadingAssets -> PongState::InGame
-
         assert_eq!(app.world.query::<&PositionComponent>().iter(&app.world).count(), 3);
         assert_eq!(app.world.query::<&MaterialHandleComponent>().iter(&app.world).count(), 3);
         assert_eq!(app.world.query::<&VelocityComponent>().iter(&app.world).count(), 1);
@@ -396,4 +486,48 @@ mod test_pong_plugin {
             assert_eq!(velocity.0.length(), 1.0);
         }
     }
+
+    #[test]
+    fn test_collision_system() {
+        let mut app = initialize_pong_plugin_and_load_assets();
+
+        let new_ball_position = Vec4::new(0.0, 0.0, 0.0, (ARENA_LENGTH + GOAL_OFFSET_FROM_ARENA - 0.0001)/2.);
+
+        let mut ball_query = app.world.query::<(&mut PositionComponent, &mut VelocityComponent, &BallComponent)>();
+        for (mut position, mut velocity, _) in ball_query.iter_mut(&mut app.world) {
+            position.0 = new_ball_position;
+            velocity.0 = Vec4::new(0.0, 0.0, 0.0, 1.0);
+        }
+        let mut ball_query = app.world.query::<(&PositionComponent, &VelocityComponent, &BallComponent)>();
+        for (position, velocity, _) in ball_query.iter(&mut app.world) {
+            assert_eq!(position.0, new_ball_position);
+            assert_eq!(velocity.0, Vec4::new(0.0, 0.0, 0.0, 1.0));
+        }
+        app.update(); // Ball should have moved by more than 0.0001, so collision system should run, and player should have scored.
+        app.update(); // Call a second time to ensure that collision system has run after ball movement
+
+        let mut ball_query = app.world.query::<(&PositionComponent, &VelocityComponent, &BallComponent)>();
+        for (position, velocity, _) in ball_query.iter(&mut app.world) {
+            assert!(position.0.distance(Vec4::ZERO) < 0.1);
+            assert!(velocity.0.w == 1.0 || velocity.0.w == -1.0, "Velocity w component should be +1 or -1 but is: {}", velocity.0.w);;
+            assert_eq!(velocity.0.length(), 1.0);
+        }
+
+    }
+
+    #[test]
+    fn test_is_goal_collision() {
+        assert_eq!(is_goal_collision(Vec4::ZERO), None);
+        assert_eq!(is_goal_collision(Vec4::new(0.0, 0.0, 0.0, (ARENA_LENGTH + GOAL_OFFSET_FROM_ARENA)/2.)), Some(Player::Blue));
+        assert_eq!(is_goal_collision(Vec4::new(0.0, 0.0, 0.0, -(ARENA_LENGTH + GOAL_OFFSET_FROM_ARENA)/2.)), Some(Player::Red));
+        assert_eq!(is_goal_collision(Vec4::new(1.0, 2.0, 5.0, (ARENA_LENGTH + GOAL_OFFSET_FROM_ARENA + 100.)/2.)), Some(Player::Blue));
+        assert_eq!(is_goal_collision(Vec4::new(-5.0, -200.0, 6000.0, -(ARENA_LENGTH + GOAL_OFFSET_FROM_ARENA + 100.)/2.)), Some(Player::Red));
+    }
+
+    #[test]
+    fn test_get_color_from_w() {
+        assert_eq!(get_color_from_w(-ARENA_LENGTH/2., ARENA_LENGTH), Color::BLUE.as_hsla());
+        assert_eq!(get_color_from_w(ARENA_LENGTH/2., ARENA_LENGTH), Color::RED.as_hsla());
+    }
+
 }
